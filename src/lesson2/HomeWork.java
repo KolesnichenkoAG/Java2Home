3f8cbcbad1de52cb2171f3988312787ae0ddbb50
KolesnichenkoAG <kag.1981@mail.ru>
2022-04-12 23:19:20 +0600

    начало

---
+++ src/lesson2/HomeWork.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+    }
+
+    public void metodM () {
+    String [][] strArr = new String [4][4];
+
+    }
+}
